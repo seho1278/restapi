61cb7121e40dab395411ebd4b74fb14a0542b326
seho1278 <seho1278@naver.com>
2023-12-14 04:47:55 +0900

    restframwork 설치 및 article CRUD 구현

---
+++ restapi/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Article, Comment
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+        
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Article, Comment
+from .serializers import ArticleListSerializer, ArticleSerializer
+
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def article_list(request):
+    if request.method == 'GET':
+        # 게시글 목록 조회
+        articles = Article.objects.all()
+        # 직렬화(다중 객체는 many=True 설정)
+        serializer = ArticleListSerializer(articles, many=True)
+        # JSON으로 응답
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        # 사용자 데이터를 받아 직렬화
+        serializer = ArticleSerializer(data=request.data)
+        # 유효성 검사
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            # 성공시 201 응답
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # 실패시 400 응답
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def article_detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
